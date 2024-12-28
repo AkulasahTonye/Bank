dfe4a96c0158329835e7e58e4c7651b6084990a8
Ghostpappi <tonyeakulasah@gmail.com>
2024-12-29 00:02:30 +0100

    Updated

--- Bank.go
+++ Bank.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	AccountBalance := 1000
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do")
 	fmt.Println("1. Check Balance")
@@ -11,10 +12,11 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var Choice int
-
-	fmt.Print("your choice: ")
+	fmt.Print("Enter Your Choice: ")
 	fmt.Scan(&Choice)
-
 	fmt.Println("Your choice: ", Choice)
 
+	if Choice == 1 {
+		fmt.Println("Your Account Balance is:", AccountBalance)
+	}
 }
