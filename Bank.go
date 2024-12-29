9845720ce200fb90b694b0d70f4f1d35f7e371d3
Ghostpappi <tonyeakulasah@gmail.com>
2024-12-29 01:32:03 +0100

    first commit

--- Bank.go
+++ Bank.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	AccountBalance := 1000
+	AccountBalance := 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do")
 	fmt.Println("1. Check Balance")
@@ -17,6 +18,22 @@ func main() {
 	fmt.Println("Your choice: ", Choice)
 
 	if Choice == 1 {
-		fmt.Println("Your Account Balance is:", AccountBalance)
+		fmt.Println("Your Account Balance:", AccountBalance)
+		fmt.Scan(&AccountBalance)
+
+	} else if Choice == 2 {
+		var depositAmount float64
+		fmt.Print("Money Deposited:")
+		fmt.Scan(&depositAmount)
+		AccountBalance += depositAmount // same thing as setting AccountBalance = AccountBalance + DepositAmount
+		fmt.Println("Updated Account Balance:", AccountBalance)
+
+	} else if Choice == 3 {
+		fmt.Print("How much do you want to Withdraw:")
+		var MoneyWithdraw float64
+		fmt.Scan(&MoneyWithdraw)
+		AccountBalance -= MoneyWithdraw
+		fmt.Println("Your Current Account Balance is:", AccountBalance)
 	}
+
 }
