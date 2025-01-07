245ebeb6aba12eff2f04c6edbe70d49c6f76179b
Ghostpappi <tonyeakulasah@gmail.com>
2025-01-07 14:14:07 +0100

    added

--- Bank.go
+++ Bank.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
 
 func main() {
-	AccountBalance := 1000.0
+	accountBalance, err := getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR MESSAGE!!")
+		fmt.Println("---------------")
+		fmt.Println(err)
+		//panic("sorry unable to continue!!")
+	}
 
 	fmt.Println("Welcome to Go Bank!")
 
@@ -17,24 +31,22 @@ func main() {
 		var Choice int
 		fmt.Print("Enter Your Choice: ")
 		fmt.Scan(&Choice)
-		fmt.Println("Your choice: ", Choice)
 
 		switch Choice {
 		case 1:
-			fmt.Println("Your Account Balance:", AccountBalance)
-			fmt.Scan(&AccountBalance)
+			fmt.Println("Your Account Balance is:", accountBalance)
 
 		case 2:
 			var depositAmount float64
 			fmt.Print("Money Deposited:")
 			fmt.Scan(&depositAmount)
-
 			if depositAmount <= 0 {
 				fmt.Println("Invalid Amount. Must be greater than 0.")
 				continue
 			}
-			AccountBalance += depositAmount // same thing as setting AccountBalance = AccountBalance + DepositAmount
-			fmt.Println("Updated Account Balance:", AccountBalance)
+			accountBalance += depositAmount // same thing as setting AccountBalance = AccountBalance + DepositAmount
+			fmt.Println("Updated Account Balance:", accountBalance)
+			writeBalanceToFile(accountBalance)
 
 		case 3:
 			fmt.Print("Withdraw Amount:")
@@ -45,21 +57,42 @@ func main() {
 				fmt.Println("Invalid Amount. Must be Greater than 0")
 				continue
 			}
-			if withdrawalAmount > AccountBalance {
+			if withdrawalAmount > accountBalance {
 				fmt.Println("You can't withdraw more than what you have.")
 				continue
 			}
 
-			AccountBalance -= withdrawalAmount // same thing as setting AccountBalance = AccountBalance - WithdrawalAmount
-			fmt.Println("Updated Account Balance:", AccountBalance)
+			accountBalance -= withdrawalAmount // same thing as setting AccountBalance = AccountBalance - WithdrawalAmount
+			fmt.Println("Updated Account Balance:", accountBalance)
+			writeBalanceToFile(accountBalance)
 
 		default:
 			println("Goodbye!")
 			fmt.Println("Thank you for choosing our Bank!")
 			return
-
 		}
-		
+	}
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	err := os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+	if err != nil {
+		return
+	}
+}
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	if err != nil {
+		return 0, errors.New("failed to get data input")
 	}
 
+	balanceTicket := string(data)
+	balance, err := strconv.ParseFloat(balanceTicket, 64)
+	if err != nil {
+		return 0, errors.New("input invalid")
+	}
+	return balance, err
 }
